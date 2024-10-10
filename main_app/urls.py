c5b98c48a937dc77f50cb494591ff8b5fa2acad8
maviles <maviles221@gmail.com>
2024-10-10 13:36:19 -0700

    sign up functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import SignUpView
 from . import views
 
 urlpatterns = [
@@ -14,5 +15,5 @@ urlpatterns = [
     path('dose/<int:dose_id>/add-comment/', views.add_comment, name='add-comment'),
     path('dose/<int:dose_id>/edit-comment/<int:comment_id>/', views.edit_comment, name='edit-comment'),
     path('dose/<int:dose_id>/delete-comment/<int:comment_id>/', views.delete_comment, name='delete-comment'),
-    path('doses/upload', views.upload, name='upload'),
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),
 ]
